0cf1298dfde8afb245618a17016fe144e466b266
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-10-20 17:46:59 +0700

    Refactor Home component to add background video

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div>
+        <div>
+          <video
+            autoPlay
+            muted
+            loop
+            className="absolute right-0 top-0 h-[700px] w-full object-cover z-[-1]"
+          >
+            <source src={MainVideo} />
+          </video>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;

c8e16d7d63d3010323b21a4e7a0ecaa4a84faa2a
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-10-21 22:35:49 +0700

    Refactor Hero component to add search form and price range input

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,81 @@
 import React from "react";
+import { useState } from "react";
 
 const Hero = () => {
+  const [priceValue, setPriceValue] = useState(0);
+
   return (
     <div className="bg-black/20 h-full">
       <div className="h-full flex justify-center items-center p-4 bg-primary/10">
         <div className="container grid grid-cols-1 gap-4">
-          {/* text content section */}
+          {/* Bagian Teks */}
           <div className="text-white">
-            <p>Our package</p>
+            <p>Paket Kami</p>
             <p
               data-aos="fade-up"
               data-aos-delay="300"
               className="font-bold text-3xl"
             >
-              Search Your Destination
+              Cari Destinasi Anda
             </p>
+          </div>
+
+          {/* Bagian Form */}
+          <div className="space-y-4 bg-white rounded-md p-4 relative">
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 py-3">
+              {/* Input Destinasi */}
+              <div>
+                <label htmlFor="destination" className="opacity-70">
+                  Cari Destinasi Anda
+                </label>
+                <input
+                  type="text"
+                  name="destination"
+                  id="destination"
+                  placeholder="Labuan Bajo"
+                  className="w-full bg-gray-100 my-2 range accent-primary 
+                  focus:outline-primary focus:outline outline-1 rounded-full p-2"
+                />
+              </div>
+
+              {/* Input Tanggal */}
+              <div>
+                <label htmlFor="date" className="opacity-70">
+                  Tanggal
+                </label>
+                <input
+                  type="date"
+                  name="date"
+                  id="date"
+                  className="w-full bg-gray-100 my-2 
+                  focus:outline-primary focus:outline outline-1 rounded-full p-2"
+                />
+              </div>
+
+              {/* Input Harga Maksimal */}
+              <div className="flex flex-col justify-between">
+                <label htmlFor="maxPrice" className="opacity-70">
+                  <div className="flex justify-between items-center">
+                    <p>Harga Maksimal</p>
+                    <p className="font-bold text-xl">Rp {priceValue}</p>
+                  </div>
+                </label>
+                <div className="bg-gray-100 rounded-full p-2 mb-2 flex items-center justify-center">
+                  <input
+                    type="range"
+                    name="maxPrice"
+                    id="maxPrice"
+                    className="appearance-none w-full bg-gradient-to-r from-primary to-secondary h-2 
+                    rounded-full my-2"
+                    min="150000"
+                    max="10000000"
+                    value={priceValue}
+                    step="10000"
+                    onChange={(e) => setPriceValue(e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
